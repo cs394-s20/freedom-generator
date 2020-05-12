79c197678c90397faa4df1960071128c324127a5
Bradley Ramos <bradleyramos2021@u.northwestern.edu>
2020-05-12 13:21:14 -0500

    display and set criteria

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import './App.css';
 import {get_idocData} from './stub-idoc';
 import { useForm } from 'react-hook-form';
 import { TextField, Button } from '@material-ui/core';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
+import { green, red } from '@material-ui/core/colors';
 import { white } from 'color-name';
 
 function App() {
   console.log(process.env.REACT_APP_API_URL);
   const [idocNum, setIdocNum] = useState("");
 
+  // Select icon based on return (innocent until proven guilty, unless they were already guilty..?)
+  const [passed, setPassed] = useState([]);
+  // Add the id to the array of passed items if it doesn't exist but if it does exist remove it
+  const handleCriteriaChange = (id) => {
+    let result =  passed.includes(id) ? passed.filter(test => test != id) : [...passed, id]
+    setPassed(result)
+   // change <CheckCircleIcon /> to <CloseRoundedIcon /> at "id"
+}
+
 
   const { register, handleSubmit, errors } = useForm();
   // callback function for check eligibility.
+  const [submitted, setSubmitted] = useState('');
   const onSubmit = data => {
     console.log(data);
+    setSubmitted('True');
+    handleCriteriaChange(101);
   };
 
   const handleChange = (event) =>{
@@ -35,14 +50,30 @@ function App() {
           { errors.IDOC_Number && <p>IDOC Number is required.</p> }
           <br />
           <br />
-          <div className="criteria">
-            <h5>Medical Furlough</h5>
-            <h5>Over 55 years old</h5>
-            <h5>12 months or less left on sentence</h5>
-            <h5>Served 25% of prison term</h5>
-            <h5>Convicted of class 2, 3, or 4 felony offense</h5>
-            <h5>Not convicted of exclusive event</h5>
-          </div>
+          {submitted && 
+            <div className="criteria">
+              <div className="criterion">Medical furlough
+              {/* Here, we check the "passed" array to see if this particular test was passed.
+                  If the given id exists in that array, we set the icon accordingly */}
+                {passed.includes(101) ? <CheckCircleIcon style={{ color: green[500] }}/> : <CloseRoundedIcon style={{ color: red[500] }}/> }
+              </div>
+              <div className="criterion">Over 55 years old
+                {passed.includes(102) ? <CheckCircleIcon style={{ color: green[500] }}/> : <CloseRoundedIcon style={{ color: red[500] }}/> }
+              </div>
+              <div className="criterion">12 months or less left on sentence
+                {passed.includes(103) ? <CheckCircleIcon style={{ color: green[500] }}/> : <CloseRoundedIcon style={{ color: red[500] }}/> }
+              </div>
+              <div className="criterion">Served 25% of prison term
+                {passed.includes(104) ? <CheckCircleIcon style={{ color: green[500] }}/> : <CloseRoundedIcon style={{ color: red[500] }}/> }
+              </div>
+              <div className="criterion">Convicted of class 2, 3, or 4 felony offense
+                {passed.includes(105) ? <CheckCircleIcon style={{ color: green[500] }}/> : <CloseRoundedIcon style={{ color: red[500] }}/> }
+              </div>
+              <div className="criterion">Not convicted of excluded event
+                {passed.includes(106) ? <CheckCircleIcon style={{ color: green[500] }}/> : <CloseRoundedIcon style={{ color: red[500] }}/> }
+              </div>
+            </div>}
+          
           <Button type="submit" variant="contained" >Submit</Button>
         </form>
         <div id="jsonData"></div>
