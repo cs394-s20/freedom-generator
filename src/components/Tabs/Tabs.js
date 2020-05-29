1f653eda1df344e87aa56c1f02c76c19e464c9c9
petersenea <Petersenea12@gmail.com>
2020-05-29 16:43:27 -0500

    tabs for mobile

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Grid } from '@material-ui/core';
+import { Button, Grid, Menu, MenuItem } from '@material-ui/core';
 import { useLocation } from 'react-router-dom';
 import './Tabs.scss';
 
@@ -8,37 +8,91 @@ import './Tabs.scss';
 function Tabs(props) {
     var computeData = props.computeData;
     var criteria = props.criteria;
+    console.log(window.screen.width);
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
     return (
         <div>
+            {window.screen.width > 412 ?
+                <Grid className="tabs-fullscreen" direction="row">
 
-            <Grid className="tabs-fullscreen" direction="row">
-
-                {/* COMPONENT is EligibilityCriteria.js */}
-                <Link className="tabs-button" to={{
-                    pathname: "/criteria" + "?computeData=" + JSON.stringify(computeData)
-                }}
-                    target="_blank">
-                    Eligibility Criteria
-                </Link>
+                    {/* COMPONENT is EligibilityCriteria.js */}
+                    <Link className="tabs-button" to={{
+                        pathname: "/criteria" + "?computeData=" + JSON.stringify(computeData)
+                    }}
+                        target="_blank">
+                        Eligibility Criteria
+                    </Link>
 
 
-                {/* COMPONENT is ExampleEmail.js */}
-                <Link className="tabs-button" to={{
-                    pathname: "/example"
-                }}
-                    target="_blank">
-                    Example Email
-                </Link>
-                
+                    {/* COMPONENT is ExampleEmail.js */}
+                    <Link className="tabs-button" to={{
+                        pathname: "/example"
+                    }}
+                        target="_blank">
+                        Example Email
+                    </Link>
 
-                {/* COMPONENT is FAQ.js */}
-                <Link className="tabs-button" to={{
-                    pathname: "/faq"
-                }}
-                    target="_blank">
-                    FAQ
-                </Link>
-            </Grid>
+
+                    {/* COMPONENT is FAQ.js */}
+                    <Link className="tabs-button" to={{
+                        pathname: "/faq"
+                    }}
+                        target="_blank">
+                        FAQ
+                    </Link>
+                </Grid>
+                :
+                <div>
+                    <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                        Open Menu
+                    </Button>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem >
+                            <Link to={{
+                                pathname: "/criteria" + "?computeData=" + JSON.stringify(computeData)
+                            }}
+                                target="_blank">
+                                Eligibility Criteria
+                            </Link>
+                        </MenuItem>
+                        <MenuItem >
+                            <Link to={{
+                                pathname: "/example"
+                            }}
+                                target="_blank">
+                                Example Email
+                            </Link>
+                        </MenuItem>
+                        <MenuItem >
+                            <Link to={{
+                                pathname: "/faq"
+                            }}
+                                target="_blank">
+                                FAQ
+                            </Link>
+                        </MenuItem>
+                    </Menu>
+                </div>
+
+
+            }
+
 
         </div>
     )
