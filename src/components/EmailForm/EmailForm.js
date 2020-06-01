8e7789377ea9424f29d41a79c8101846eebff356
petersenea <Petersenea12@gmail.com>
2020-06-01 18:21:24 -0500

    tabs are no longer clickable when modal is open

--- src/components/EmailForm/EmailForm.js
+++ src/components/EmailForm/EmailForm.js
@@ -38,10 +38,13 @@ function EmailForm(props) {
   if (modalOpen) {
     console.log(data)
     document.getElementById("overlay").style.opacity = 0.7;
+    document.getElementById("overlay").style.pointerEvents = "none";
     modal = (<Modal setModalOpen={setModalOpen} data={data} />);
   }
   else if (document.getElementById("overlay")) {
     document.getElementById("overlay").style.opacity = 1;
+    document.getElementById("overlay").style.pointerEvents = "auto";
+
 
   }
 
@@ -50,94 +53,91 @@ function EmailForm(props) {
 
   return (
     <div>
-      <Tabs computeData={location.state.computeData} />
-      <div>
-
-        <div id="overlay">
-          <div className="Email">
-            <div>
-              <h1>Step 3 of 3</h1>
-              <h2>Fill in the form below to generate an email petitioning for early release.</h2>
-              <br/>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <Grid container direction="row" alignItems="center">
-                  <Grid><Typography>Dear&nbsp;</Typography></Grid>
-                  <Grid><TextField name="wardenName" placeholder="warden name" inputRef={register({ required: true })} /></Grid>,
+      <div id="overlay">
+        <Tabs computeData={location.state.computeData} />
+        <div className="Email">
+          <div>
+            <h1>Step 3 of 3</h1>
+            <h2>Fill in the form below to generate an email petitioning for early release.</h2>
+            <br />
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Grid container direction="row" alignItems="center">
+                <Grid><Typography>Dear&nbsp;</Typography></Grid>
+                <Grid><TextField name="wardenName" placeholder="warden name" inputRef={register({ required: true })} /></Grid>,
               </Grid>
-                <br />
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  {inmateName} ({location.state.computeData.idocNumber}) is my&nbsp;
-                <TextField style={{marginLeft:'10px'}} name="relation" placeholder="relation to writer" inputRef={register({ required: true })} />
+              <br />
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                {inmateName} ({location.state.computeData.idocNumber}) is my&nbsp;
+                <TextField style={{ marginLeft: '10px' }} name="relation" placeholder="relation to writer" inputRef={register({ required: true })} />
                 and is eligible for transfer to home detention pursuant to {location.state.statuteNumber}.
               </Grid>
-                <br />
-                <Grid container alignItems="center" textAlign="left" justify="flex-start" style={{ lineHeight: 2 }}>
-                  I am writing to urge you to place {inmateName} on home detention as soon as possible.&nbsp;
+              <br />
+              <Grid container alignItems="center" textAlign="left" justify="flex-start" style={{ lineHeight: 2 }}>
+                I am writing to urge you to place {inmateName} on home detention as soon as possible.&nbsp;
                 {inmateName} is an ideal candidate for home detention because &nbsp;
                 <TextField name="reason1" style={{ width: 300 }} placeholder="first reason for release" inputRef={register({ required: true })} />,&nbsp;
                 <TextField name="reason2" style={{ width: 300 }} placeholder="second reason for release" inputRef={register({ required: true })} />, and &nbsp;
                 <TextField name="reason3" style={{ width: 300 }} placeholder="third reason for release" inputRef={register({ required: true })} />.
 
               </Grid>
-                <Grid><br /></Grid>
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  If {inmateName} is transferred to home detention, they can live with&nbsp;
+              <Grid><br /></Grid>
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                If {inmateName} is transferred to home detention, they can live with&nbsp;
                 <TextField name="liveWith" placeholder="name" inputRef={register({ required: true })} />,&nbsp;
                 <TextField name="relationLiveWith" placeholder="relation to inmate" inputRef={register({ required: true })} />,&nbsp;
                 <TextField name="phoneLiveWith" placeholder="phone number" inputRef={register({ required: true })} />,&nbsp;
                 <TextField name="addressLiveWith" placeholder="address" inputRef={register({ required: true })} />.&nbsp;
               </Grid>
-                <br />
+              <br />
 
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  {inmateName} will receive support in the form of
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                {inmateName} will receive support in the form of
               </Grid>
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  <Checkbox name="parole" inputRef={register} color="primary"/>
-                  <label style={{ fontWeight: "bold" }}>Assistance complying with parole requirements</label>
-                </Grid>
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  <Checkbox name="groceries" inputRef={register} color="primary"/>
-                  <label style={{ fontWeight: "bold" }}>Groceries</label>
-                </Grid>
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  <Checkbox name="job" inputRef={register} color="primary"/>
-                  <label style={{ fontWeight: "bold" }}>Job placement</label>
-                </Grid>
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  <Checkbox name="medical" inputRef={register} color="primary"/>
-                  <label style={{ fontWeight: "bold" }}>Assistance meeting medical needs</label>
-                </Grid>
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  from the following individuals / entities:&nbsp; <TextField name="supportAndContact" style={{ width: 300 }} placeholder="name(s) and contact information" inputRef={register({ required: true })} />.
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                <Checkbox name="parole" inputRef={register} color="primary" />
+                <label style={{ fontWeight: "bold" }}>Assistance complying with parole requirements</label>
               </Grid>
-                <Grid><br /></Grid>
-                <Grid container alignItems="center" justify="center" style={{ lineHeight: 2 }}>
-                  <TextField name="threeSentences" style={{ width: 600 }} multiline inputRef={register({ required: true })} rows={4} variant="outlined" placeholder="Three Sentences here regarding how prisoner’s incarceration has affected family members and community and why/how prisoner will contribute positively to the community upon release. " />
-                </Grid>
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                <Checkbox name="groceries" inputRef={register} color="primary" />
+                <label style={{ fontWeight: "bold" }}>Groceries</label>
+              </Grid>
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                <Checkbox name="job" inputRef={register} color="primary" />
+                <label style={{ fontWeight: "bold" }}>Job placement</label>
+              </Grid>
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                <Checkbox name="medical" inputRef={register} color="primary" />
+                <label style={{ fontWeight: "bold" }}>Assistance meeting medical needs</label>
+              </Grid>
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                from the following individuals / entities:&nbsp; <TextField name="supportAndContact" style={{ width: 300 }} placeholder="name(s) and contact information" inputRef={register({ required: true })} />.
+              </Grid>
+              <Grid><br /></Grid>
+              <Grid container alignItems="center" justify="center" style={{ lineHeight: 2 }}>
+                <TextField name="threeSentences" style={{ width: 600 }} multiline inputRef={register({ required: true })} rows={4} variant="outlined" placeholder="Three Sentences here regarding how prisoner’s incarceration has affected family members and community and why/how prisoner will contribute positively to the community upon release. " />
+              </Grid>
 
-                <Grid><br /></Grid>
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  If I can provide you with any further information about this request for transfer to home detention, please contact me at&nbsp;
+              <Grid><br /></Grid>
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                If I can provide you with any further information about this request for transfer to home detention, please contact me at&nbsp;
                 <TextField name="email" placeholder="email" inputRef={register({ required: true })} />&nbsp;or&nbsp;
                 <TextField name="phone" placeholder="phone number" inputRef={register({ required: true })} />.
               </Grid>
-                <br />
-                <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
-                  I will contact your office to set a time to discuss this request within the next week.
+              <br />
+              <Grid container alignItems="center" justify="flex-start" style={{ lineHeight: 2 }}>
+                I will contact your office to set a time to discuss this request within the next week.
               </Grid>
-                <Grid container alignItems="center" justify="flex-start"><br />Thank you for your consideration. </Grid>
-                <Grid container alignItems="center" justify="flex-start"><TextField name="submitter" placeholder="name of submitter" inputRef={register({ required: true })} /></Grid>
-                <div className="marginTop">
+              <Grid container alignItems="center" justify="flex-start"><br />Thank you for your consideration. </Grid>
+              <Grid container alignItems="center" justify="flex-start"><TextField name="submitter" placeholder="name of submitter" inputRef={register({ required: true })} /></Grid>
+              <div className="marginTop">
                 <Button className="button" type="submit" variant="contained" color="primary">Preview Email</Button>
-                </div>
-              </form>
-            </div>
+              </div>
+            </form>
           </div>
-
         </div>
-        {modal}
-      </div >
+
+      </div>
+      {modal}
     </div>
   );
 }
